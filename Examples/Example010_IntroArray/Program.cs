8fd9e09989a4d70af8655e2a89a4abf8d1ada031
Ulysses <morozovvladislav@yandex.ru>
2023-02-04 13:29:28 +0400

    Example010_IntroArray

---
+++ Examples/Example010_IntroArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {1,2,3,4,5,6,8,9};
+int n = array.Length;
+int find = 6;
+int index = -1;
+while(index < n)
+{
+    if(index == find)
+    {
+        Console.Write(array[index]);
+        break;
+    }
+    index++;
+}
