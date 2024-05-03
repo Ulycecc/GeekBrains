fdcc005392eb787528620e5104fc79d510fecd45
Ulysses <morozovvladislav@yandex.ru>
2024-05-03 16:54:21 +0400

    'HW5'

--- Django/online_store/myapp/models.py
+++ Django/online_store/myapp/models.py
@@ -12,12 +12,20 @@ class Сonsumer(models.Model):
         return f'Name: {self.name}, email: {self.email}, phone_number: {self.phone_number},  address: {self.address}'
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField(max_length=5)
     date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'Name: {self.name}, description: {self.description}, price: {self.price},  quantity: {self.quantity}'
